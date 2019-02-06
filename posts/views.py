66053d350081af0162b47c221785d91410df862d
LooDaHu <40339075+LooDaHu@users.noreply.github.com>
2019-02-06 10:38:23 -0700

    Update views.py
    #update at 02/06/2019

--- posts/views.py
+++ posts/views.py
@@ -39,3 +39,17 @@ def hello_world(request):
         return Response({"message": "Got some data!", "data": request.data})
     return Response({"message": "Hello, world!"})
 
+@api_view(['GET', 'POST'])
+def get_posts_with_time(request, time1, time2):
+    the_list = []
+    the_time1 = datetime.strptime(time1, '%Y-%m-%d').astimezone()
+    the_time2 = datetime.strptime(time2, '%Y-%m-%d').astimezone()
+    queryset = Post.objects.all()
+    for post in queryset:
+        if the_time1 <= post.created_at:
+            if the_time2 >= post.created_at:
+                the_list.append(post)
+
+    serializer = PostSerializer(the_list, many=True)
+    return Response(serializer.data)
+
