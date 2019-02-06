bddc5225e6aae27cc571cc89f0d742d2b7259a0a
LooDaHu <40339075+LooDaHu@users.noreply.github.com>
2019-02-06 10:40:08 -0700

    Update urls.py
    #update 02/06/2019

--- posts/urls.py
+++ posts/urls.py
@@ -6,6 +6,7 @@ urlpatterns = (
     path('<int:pk>/', views.PostDetail.as_view()),
     path('hello', views.hello_world),
     path('time_gap/<int:pk1>&<int:pk2>', views.get_time_gap, name='Time Gap')
+    path('time_query/<str:time1>&<str:time2>', views.get_posts_with_time)
 )
 
 
